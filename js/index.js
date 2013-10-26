fa0c2b730dc84672613d7624a21f0eb136eb8f39
Vitaliy Filipov <altras@gmail.com>
2013-10-27 02:51:09 +0300

    added media queries

--- js/index.js
+++ js/index.js
@@ -1,37 +1,56 @@
 $(document).ready(function() {
-	submitButton.addEventListener(function(e){
-		debugger;
-	});
+  $.ajax({
+    type: "GET",
+    url : APIendpoint + "feedback/?key=hackfmi",
+    dataType: "json",
+    success : function(data) {
+      // debugger;
+      $("#recaptcha").append(data.recaptcha);
+    },
+    error : function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus + ' ' + errorThrown);
+    }
+  });
 });
+
 var APIendpoint = "http://146.185.165.209/server/";
-var currentCourse;
 var onKeypress = function(e) {
 	var tmp_str = $("#courseInputField").val();
-	if (tmp_str.length > 3) {
+  if (tmp_str.length > 3) {
 		$.ajax({
-		type : "GET",
-		url : APIendpoint + "startWith/" + tmp_str,
-	 	dataType: 'json',
-		success : function(data) {
-			debugger;
-			var options = []
-			for(var i=0; i < data.length; i++) {
-				options.push(data[i].name);
-			}
-			$("#courseInputField").autocomplete({
-				source: options
-			});
-		},
-
-		error : function(jqXHR, textStatus, errorThrown) {
-			console.log(textStatus + ' ' + errorThrown);
-		}
+  		type : "GET",
+  		url : APIendpoint + "startsWith/" + tmp_str,
+  	 	dataType: 'json',
+  		success : function(data) {
+        var context = data;
+        var options = [];
+        for (var i = data.length - 1; i >= 0; i--) {
+          options.push(data[i].name);
+        };
+  			$("#courseInputField").autocomplete({
+  				source: options,
+          autoFocus: true,
+  				select: function(e, ui) {
+            var courseID = -1;
+            for(var i = 0, len = context.length; i<len; i++) {
+              if (context[i].name === ui.item.label) {
+                courseID = context[i].uid;
+                break;
+              }
+            };
+            $("#courseId").attr("value", courseID);
+            getTeachersForCourse(courseID)
+  				}
+  			})},
+  		error : function(jqXHR, textStatus, errorThrown) {
+  			console.log(textStatus + ' ' + errorThrown);
+  		}
 		});
 	}
 }
-var getTeachersForCourse = function(e) {
+var getTeachersForCourse = function(label) {
 	//GET Teachers
-	var courseID = $();
+	var courseID = label;
 	$.ajax({
 		type : "GET",
 		url : APIendpoint + "teacherByCourse/" + courseID,
@@ -58,25 +77,36 @@ var postFeedback = function(e){
 	value = $("#teacher").val()
 	value = $("#positiveFeedback").val()
 	value = $("#negativeFeedback").val()
-	// $.ajax({
-	// 	type : "POST",
-	// 	url : APIendpoint,
-	// 	data : {
-	// 		"course": $("#courseInputField").val(),
-	// 		"teacher": $("#teacher").val(),
-	// 		"positiveFeedback": $("#positiveFeedback").val(),
-	// 		"negativeFeedback": $("#negativeFeedback").val(),
-
-	// 		// "gamified" : payload,
-	// 		// "feedbackId" : feedbackId,
-	// 		// "studentId" : studentId
-	// 	},
-	// 	success : function(data) {
-	// 		$("#gamifiedEducation").hide("fast");
-	// 		$("#thankYouMessage").show("fast");
-	// 	},
-	// 	error : function(jqXHR, textStatus, errorThrown) {
-	// 		console.log(textStatus + ' ' + errorThrown);
-	// 	}
-	// });
+	$.ajax({
+		type : "POST",
+		url : APIendpoint +"feedback/?key=hackfmi",
+		data : {
+			"teacher_id":     $("#teacher").val(),
+      "course_id":      $("#courseId").val(),
+			"positive":       $("#positiveFeedback").val(),
+			"negative":       $("#negativeFeedback").val(),
+      "course_rating":  0,
+      "teacher_rating": 0
+			// "gamified" : payload,
+			// "feedbackId" : feedbackId,
+			// "studentId" : studentId
+		},
+    statusCode: {
+      404: function() {
+        alert( "page not found" );
+      },
+      default: function() {
+        console.log("default");
+      }
+    },
+		success : function(data) {
+      debugger;
+      console.log(data);
+			// $("#gamifiedEducation").hide("fast");
+			// $("#thankYouMessage").show("fast");
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			console.log(textStatus + ' ' + errorThrown);
+		}
+	});
 }
